152b74bcf3399c17315f1b8669caca478196dae7
djcommunity <djcommunity20@gmail.com>
2020-04-17 15:47:34 +0900

    20_04_17_JARAM

---
+++ baekjun_1330.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main()
+{
+	int a, b;
+	scanf("%d %d",&a,&b);
+
+	if (a > b) {
+		printf(">");
+	}
+	else if (a == b) {
+		printf("==");
+	}
+	else {
+		printf("<");
+	}
+return 0;
+}
